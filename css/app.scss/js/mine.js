f23199b5dda2224e62e05c0f3425acbe18a5f35c
Your Name <aleko0306@gmail.com>
2022-07-01 20:56:24 +0400

    js

--- css/app.scss/js/mine.js
+++ css/app.scss/js/mine.js
@@ -1,4 +1,21 @@
-let x = 13;
+// start
+
+var x = 10;
+var y = 10;
+
+var z = x + y;
+
+console.log (z);
+
+// end
 
 
-console.log ( x > 12  || x == 9 );
+// start
+
+// let x = 13;
+
+
+// console.log ( x > 12  || x == 9 );
+
+
+// end
